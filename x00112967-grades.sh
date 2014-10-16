31e12f9e32059dbed715b1287e19e4c9147b1966
x00112967 <spreston2009@gmail.com>
2014-10-16 11:03:49 +0100

    grades class

---
+++ x00112967-grades.sh
@@ -0,0 +1,36 @@
+
+#Test Case 1
+#Inputs: 100 100
+#Expected Output: pass with distinction 
+#Actual Output: pass with distiction
+java grades 100 100
+
+#Test Case 2
+#Inputs: 80 129
+#Expected Output: invalid input
+#Actual Output: invalid output
+java grades 80 129
+
+#Test Case 3
+#Inputs: 80 80 
+#Expected Outputs: pass with distinction
+#Actual Outputs: pass
+java grades 80 80
+
+#Test Case 4
+#Inputs: 50 50
+#Expected Outputs: pass
+#Actual Outputs: fail
+java grades 50 50
+
+#Test Case 5
+#Inputs: 90 79
+#Expected Outputs: pass
+#Actual Outputs: pass with disticntion 
+java grades 90 79
+
+#Test Case 6
+#Inputs: 30 100
+#Expected Outputs: component fail 
+#Actual Outputs: component fail
+java grades 30 100
